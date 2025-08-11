e869d2986bc3a762556d7cfc292bf9bef2652ef8
Katarzyna Poręba <79569248+katporeba@users.noreply.github.com>
2025-08-11 16:52:13 +0200

    Update inference_VQ_Diffusion.py

--- inference_VQ_Diffusion.py
+++ inference_VQ_Diffusion.py
@@ -172,7 +172,7 @@ if __name__ == '__main__':
     )
 
     for idx, (caption, img_id) in enumerate(zip(captions, image_ids)):
-        if idx < 1135:
+        if idx < 1241:
           continue
         print(f"[{idx+1}/{len(captions)}] Generuję obrazy dla: '{caption}'")
 
